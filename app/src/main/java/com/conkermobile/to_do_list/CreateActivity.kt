edee2556b0204e8deda5286d379751cfd036e329
ruo20 <ruo2019@outlook.com>
2020-10-01 17:54:39 -0700

    Add button

--- app/src/main/java/com/conkermobile/to_do_list/CreateActivity.kt
+++ app/src/main/java/com/conkermobile/to_do_list/CreateActivity.kt
@@ -1,5 +1,6 @@
 package com.conkermobile.to_do_list
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,10 @@ class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+    }
+
+    private fun clickButton() {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/conkermobile/to_do_list/MainActivity.kt
+++ app/src/main/java/com/conkermobile/to_do_list/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.conkermobile.to_do_list
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -10,9 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
         findViewById<FloatingActionButton>(R.id.FloatingActionButton).setOnClickListener { _ ->
-
+            var intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
         }
     }
 }
