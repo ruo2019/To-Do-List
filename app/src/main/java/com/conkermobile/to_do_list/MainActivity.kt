898728d46649318e25e8986ae2dcde4679773277
ruo20 <ruo2019@outlook.com>
2020-10-06 18:27:54 -0700

    test

--- app/src/main/java/com/conkermobile/to_do_list/MainActivity.kt
+++ app/src/main/java/com/conkermobile/to_do_list/MainActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.conkermobile.to_do_list.DATA_ITEMS
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.conkermobile.to_do_list.util.Items
 
 class MainActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance().currentUser?.uid
+    private val firebaseDB = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<FloatingActionButton>(R.id.FloatingActionButton).setOnClickListener { _ ->
-            var intent = Intent(this, CreateActivity::class.java)
+            val intent = Intent(this, CreateActivity::class.java)
             startActivity(intent)
             finish()
         }
